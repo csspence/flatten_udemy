efa57d3347a95eb072d39c7365fb12c3f4adac1b
Crew Spence <csspence123@gmail.com>
2020-07-14 06:12:38 -0500

    Solution doesn't work yet.  Ran out of time.

--- flatten_udemy.js
+++ flatten_udemy.js
@@ -3,12 +3,22 @@ JavaScript Algorithms and Data Structures Masterclass
 
 flatten
 Write a recursive function called flatten which accepts an array of arrays and returns a new array with all values flattened.
+*/
 
-function flatten(){
-
+function flatten(array){
+  let newArr = [];
+  if(array[array.length - 1].length === 1) {
+    newArr.push(array[array.length - 1]);
+    if((array.length - 1) > 1) {
+      newArr.push(flatten(array.slice(0, array.length - 1)));
+    }
+  } else {
+    newArr.push(array[array.length - 1]);
+  }
+  return newArr;
 }
 
 // flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
 // flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
 // flatten([[1],[2],[3]]) // [1,2,3]
-// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
